fadc34680f504ab0d7b9623a365932136d003cf9
Dikshant Sharma <sdikshant89@gmail.com>
2023-10-23 13:24:17 -0600

    [23 Oct, 1pm] Added packages and updated comments

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -1,10 +1,23 @@
+/*
+    This is the starting point of the application, all the api hits go through this method
+
+    Separate packages installed:
+    - Nodemon (npm install --save-dev nodemon)
+    # To restart server automatically when changes are made in app
+    # Changes done to script for npm start command to run (nodemon server.js)
+
+    - Morgam (npm install --save morgan)
+    # Used to log info about the request sent to the application
+*/
 const express = require('express');
+const requestInfo = require('mrogan');
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-// This is the starting point of the application, all the api hits go through this method
+app.use(requestInfo('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
